f37e6d4391655f3091c4d0e40c4e0baaffe9ffab
Neeraj Bisht <bishtneeraj801@gmail.com>
2026-03-07 10:57:37 +0530

    Get all users except logged in route

---
+++ backend/controller/user.controller.js
@@ -0,0 +1,16 @@
+import User from "../models/user.model.js";
+
+export const getUserForSidebar = async (req, res, next) => {
+  try {
+    const loggedInUserId = req.user.id;
+
+    const allExceptLoggedIn = await User.find({
+      _id: { $ne: loggedInUserId },
+    }).select("-password");
+
+    res.status(200).json(allExceptLoggedIn);
+  } catch (error) {
+    next(error);
+  }
+};
+ 
