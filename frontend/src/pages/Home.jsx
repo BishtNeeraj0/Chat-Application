b1cb4eb3c452e3e57e9db892a511a11e4e0bd8c6
Neeraj Bisht <bishtneeraj801@gmail.com>
2026-03-10 20:21:23 +0530

    Message Container UI

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Sidebar from "../components/sidebar/sidebar";
+import MessageContainer from "../components/Messages/MessageContainer";
 
 const Home = () => {
   return (
     <div className="flex sm:h-[450px] md:h-[550px] rounded-lg overflow-hidden bg-white shadow-md">
       <Sidebar />
+      <MessageContainer/>
     </div>
   );
 };
